d0c551dc10d6598d941177cbdf8f09286226a2eb
SlivkaIgor <Slivka-97@mail.ru>
2022-02-07 17:55:24 +0300

    commit 4

--- drfsite/women/serializer.py
+++ drfsite/women/serializer.py
@@ -4,11 +4,11 @@ from rest_framework.renderers import JSONRenderer
 from women.models import Women
 
 
-#class WomenModel:
-    #def __init__(self, title, content, cat_id):
-        #self.title = title
-        #self.content = content
-        #self.cat_id = cat_id
+# class WomenModel:
+# def __init__(self, title, content, cat_id):
+# self.title = title
+# self.content = content
+# self.cat_id = cat_id
 
 
 class WomenSerializer(serializers.Serializer):
@@ -20,4 +20,9 @@ class WomenSerializer(serializers.Serializer):
         return Women.objects.create(**validated_data)
 
 
-
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get("title", instance.title)
+        instance.content = validated_data.get("content", instance.content)
+        instance.cat_id = validated_data.get("cat_id", instance.cat_id)
+        instance.save()
+        return instance
--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from setuptools._distutils.command.install_data import install_data
 
 from .models import Women
 from .serializer import WomenSerializer
@@ -21,5 +22,20 @@ class WomenAPIView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response({'post': serializer.data})
+
+    def put(self, request, *args, **kwargs):
+        pk = request.data.get("pk", None)
+        if not pk:
+            return Response({'Error': 'Method PUT not allowed'})
+        try:
+            instance = Women.objects.get(pk=pk)
+        except:
+            return Response({'Error': 'Object not exists'})
+
+        serializer = WomenSerializer(data=request.data, instance=instance)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({'pots': serializer.data})
+
     # queryset = Women.objects.all()
     # serializer_class = WomenSerializer
